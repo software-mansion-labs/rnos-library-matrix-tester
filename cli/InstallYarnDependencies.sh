c84d538aee2bebcd1210fa2f350f6f8e93d9c8e9
Tomasz Zawadzki <tomasz.zawadzki@swmansion.com>
2023-02-22 18:31:13 +0100

    Fail if yarn fails

--- cli/InstallYarnDependencies.sh
+++ cli/InstallYarnDependencies.sh
@@ -4,12 +4,16 @@ REACT_NATIVE_MINOR_VERSION=$(cut -d '.' -f 2,2 <<< $E2E_REACT_NATIVE_VERSION)
 
 cd $E2E_APP_PATH
 
+# install patch-package
+yarn add --dev patch-package postinstall-postinstall || exit 1
+
 # install SWM libraries
 yarn add \
     software-mansion/react-native-reanimated#c71bdd3ba13eaab599d033de1bfa7c45e78a5a08 \
     react-native-gesture-handler@2.9.0 \
     yarn add react-native-screens@3.19.0 \
-    yarn add react-native-svg@13.7.0
+    yarn add react-native-svg@13.7.0 \
+    || exit 1
 
 # install other libraries
 if [ $REACT_NATIVE_MINOR_VERSION -ge 65 ]; then
@@ -17,18 +21,15 @@ if [ $REACT_NATIVE_MINOR_VERSION -ge 65 ]; then
 else
     RNSAC_VERSION=3.4.1
 fi
-yarn add react-native-safe-area-context@$RNSAC_VERSION
-
-# install patch-package
-yarn add --dev patch-package postinstall-postinstall
+yarn add react-native-safe-area-context@$RNSAC_VERSION || exit 1
 
 # install linters
-yarn add --dev prettier
+yarn add --dev prettier || exit 1
 
 # install TypeScript (https://reactnative.dev/docs/typescript)
 if [ $REACT_NATIVE_MINOR_VERSION -le 70 ]; then
-    yarn add --dev @tsconfig/react-native @types/jest @types/react @types/react-test-renderer @types/react-native typescript
+    yarn add --dev @tsconfig/react-native @types/jest @types/react @types/react-test-renderer @types/react-native typescript || exit 1
 fi
 
 # install E2E testing tools
-yarn add appium webdriverio@7
+yarn add appium webdriverio@7 || exit 1
