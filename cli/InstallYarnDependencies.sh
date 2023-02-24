bd027670741b90d2a7091aa7c53ff8ad6f385ca6
Tomasz Zawadzki <tomasz.zawadzki@swmansion.com>
2023-02-24 08:28:42 +0100

    Update InstallYarnDependencies.sh

--- cli/InstallYarnDependencies.sh
+++ cli/InstallYarnDependencies.sh
@@ -12,19 +12,18 @@ yarn add \
     software-mansion/react-native-reanimated#b45469824047f5f2cfe73423576bba40ac4d3d84 \
     react-native-gesture-handler@2.9.0 \
     react-native-screens@3.19.0 \
-    react-native-svg@13.7.0 \
-    || exit 1
+    react-native-svg@13.7.0
 
 # install linters
-yarn add --dev prettier || exit 1
+yarn add --dev prettier
 
 # install TypeScript (https://reactnative.dev/docs/typescript)
 if [ $REACT_NATIVE_MINOR_VERSION -le 70 ]; then
-    yarn add --dev @tsconfig/react-native @types/jest @types/react @types/react-test-renderer @types/react-native typescript || exit 1
+    yarn add --dev @tsconfig/react-native @types/jest @types/react @types/react-test-renderer @types/react-native typescript
 fi
 
 # install E2E testing tools
-yarn add appium webdriverio@7 || exit 1
+yarn add appium webdriverio@7
 
 # install other libraries
 if [ $REACT_NATIVE_MINOR_VERSION -ge 65 ]; then
@@ -32,4 +31,4 @@ if [ $REACT_NATIVE_MINOR_VERSION -ge 65 ]; then
 else
     RNSAC_VERSION=3.4.1
 fi
-yarn add react-native-safe-area-context@$RNSAC_VERSION || exit 1
+yarn add react-native-safe-area-context@$RNSAC_VERSION
